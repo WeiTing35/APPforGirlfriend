865a27219bec3fc46dcd17985f68112b28ad194b
Wei <tim24514@gmail.com>
2020-02-09 00:01:28 +0800

    0209

--- mysirstAPP/ViewController.swift
+++ mysirstAPP/ViewController.swift
@@ -16,9 +16,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         createNotification()
         
-        
-        
-        
     }
 
     @IBAction func showMessage(sender: UIButton) {
@@ -54,8 +51,39 @@ class ViewController: UIViewController {
             content.sound = UNNotificationSound.default
             content.categoryIdentifier = "alarmMessage"
             
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-            let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+//        let date = datePicker.date
+//        let calendar = Calendar.current
+//        let components = calendar.dateComponents([ .hour, .minute],
+//            from: date)
+//        let trigger = UNCalendarNotificationTrigger(dateMatching:
+//            components, repeats: true)
+        
+//        let content = UNMutableNotificationContent()
+//        content.body = "溫蒂要下班了，要準備出門買點心接送她"
+        
+        
+        // 定义触发的时间组合
+        var matchingDate = DateComponents()
+        matchingDate.hour = 12
+        matchingDate.minute = 03
+
+        let trigger = UNCalendarNotificationTrigger(    dateMatching: matchingDate,
+                                                            repeats: true)
+        // *** 参数说明 ***
+        // dateMatching     : 触发通知的时间组合 DateComponents
+        // repeats          : 是否重复 true/false
+
+        
+        
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+
+//        let request = UNNotificationRequest(identifier:
+//            "notification1", content: content, trigger: trigger)
+//        UNUserNotificationCenter.current().add(request,
+//             withCompletionHandler: nil)
             
             UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         }
